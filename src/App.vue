d6e0eafff43ad058da844e53b3476289d4aa0662
HyungJooKim <hyungjookim12@gmail.com>
2022-01-12 16:13:42 +0900

    [♻️ refector] 폼 컴포넌트로 구성
    1. 컴포넌트 구성 및 emit 추가

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,8 @@ v-show 는 랜더링 할때 비용이 많이 들고, v-if 는 토글 할때 비�
 <template>
 <div class="container"> <!--왼쪽 오른쪽 여백을 준다.-->
  <h2>To-do List </h2>   <!--template에선 .value를 해줄필요가 없다.-->
- <!--submit 버튼이 눌려졌을때 onSubmit 함수 호출 
-     prevent : refresh 되는 속성 제거 -->
-  <form @submit.prevent="onSubmit">
-  <div class ="d-flex">
-   <div class="flex-grow-1 mr-2">
-      <input
-        class="form-control"
-        type="text"
-        v-model="todo"
-        placeholder="Type new to-do"
-      >
-    </div>
-    <div>
-    <button class="btn btn-primary"
-    type="submit"
-    >Add
-    </button> 
-</div>
-  </div>
- 
-<div v-show="hasError" style="color: red">This field cannot be empty</div>
-   </form> 
+ <TodoSimpleForm @add-todo="addTodo"/>
+
   <div v-if="!todos.length">
     추가된 Todo가 없습니다.
     </div>
@@ -62,40 +42,33 @@ v-show 는 랜더링 할때 비용이 많이 들고, v-if 는 토글 할때 비�
 <script>
 //template에서는 값의 변경을 일반 변수로는 적용할 수 없다.
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
-  setup() {
+  components: {
+    TodoSimpleForm    //component 등록 
+  },
+
+  setup() { 
     const deleteTodo = (index) => {
        todos.value.splice(index, 1);
     };
 
-    const todo = ref('');
     const todos = ref([]); 
+
     const todoStyle = {
       textDecoration: 'line-through',
       color: 'gray'
     };
-    const hasError = ref(false);
- 
-    const onSubmit = () =>{
-      if(todo.value ===''){
-        hasError.value = true;
-      } else{
-          todos.value.push({  //ref이기 떄문에 value 필요
-          id: Date.now(),   //id에 고유성을 최대한 주기 위해 
-          subject: todo.value,
-          completed: false,
-        });
-        hasError.value = false;
-        todo.value = ''; //추가가 되면 empty로 
-    }
-      };
+
+    const addTodo = (todo) => {
+      todos.value.push(todo);
+    };
+
 
     return {//template 안에서 접근 가능하게 함
-      todo,
-      onSubmit,
+      addTodo,
       todos,
-      hasError,
       todoStyle,
       deleteTodo
     };
