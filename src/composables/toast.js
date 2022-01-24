685c473bf9cca7ee098c6898becd1a1fa7afa324
HyungJooKim <hyungjookim12@gmail.com>
2022-01-24 22:25:40 +0900

    [♻️ refector] 토스트 메시지 수정
    1. 토스트 메시지 중복 가능하게 수정

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -4,19 +4,15 @@ import { useStore } from 'vuex';
 export const useToast = () => {
   const store = useStore();
   //값만 가져와선 vuex에서 값이 변하는것을 감지하지 못하여 computed로 감싸준다.
-  const toastMessage = computed(() => store.getters['toast/toastMessageWithSmile']);
-  const toastAlertType = computed(() => store.state.toast.toastAlertType);
-  const showToast = computed(() => store.state.toast.showToast);
+  const toasts = computed(() => store.state.toast.toasts);
+
 
   const triggerToast = (message, type = 'success') => {
     store.dispatch('toast/triggerToast', message, type);
   }
 
   return {
-    toastMessage,
-    toastAlertType,
-    showToast,
     triggerToast,
-
+    toasts
   }
 }
